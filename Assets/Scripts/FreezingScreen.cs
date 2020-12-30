04c2ae33e865be6edf7291ee1f231568f9690304
Achille PACE <achille.pace@laposte.net>
2020-12-30 10:55:00 +0100

    Added freezing screen

--- Assets/Scripts/FreezingScreen.cs
+++ Assets/Scripts/FreezingScreen.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FreezingScreen : MonoBehaviour
 {
-    private Camera m_MainCamera = null;
+    [SerializeField]
+    private GameObject m_Character = null;
     [SerializeField]
     private GameObject m_HotPosition = null;
     [SerializeField]
@@ -12,18 +14,21 @@ public class FreezingScreen : MonoBehaviour
     private float m_MaxOutput = 1.0f;
     [SerializeField]
     private float m_FreezingOutput = 0.0f;
+    [SerializeField]
+    private Image m_FreezingScreenImage = null;
 
 
-    private void Awake()
-    {
-        m_MainCamera = FindObjectOfType<Camera>();
-    }
 
     // Update is called once per frame
     void Update()
     {
-        float l_DistanceToHot = Vector3.Magnitude(m_MainCamera.transform.position - m_HotPosition.transform.position);
+        float l_DistanceToHot = Vector3.Magnitude(m_Character.transform.position - m_HotPosition.transform.position);
         float l_ColdIntensity = l_DistanceToHot - m_HotDistance;
         m_FreezingOutput = Mathf.Clamp(l_ColdIntensity, 0.0f, m_MaxOutput);
+
+        Color l_FreezingScreenColor = m_FreezingScreenImage.color;
+        l_FreezingScreenColor.a = m_FreezingOutput;
+
+        m_FreezingScreenImage.color = l_FreezingScreenColor;
     }
 }
